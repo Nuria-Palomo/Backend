cf705bbe2fa32a6b92bf4ee80792b300d0bfed77
Nuria Palomo <nuriapb68@gmail.com>
2023-09-21 13:42:54 +0200

    registro de usuario funcionando

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -5,7 +5,6 @@ const UserSchema = mongoose.Schema ({
 
     name: {
         type: String,
-        required: true,
         trim: true
     },
     email: {
--- front/src/pages/RegisterPage.jsx
+++ front/src/pages/RegisterPage.jsx
@@ -1,29 +1,57 @@
 import { useForm } from 'react-hook-form';
 import '../index.css';
+import React, { useState } from 'react';
+
 
 
 
 function RegisterPage() {
-    const { register, handleSubmit } = useForm();
+    const [response, setResponse] = useState(null);
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
+    const data ={
+        email: email,
+        password: password
+    }
+    const options ={
+        method: "POST", 
+        headers: {"content-type":"application/json"},
+        body: JSON.stringify(data)
+    }
+
+    const handleSubmit =  (e) => {
+        e.preventDefault()
+        
+        try {
+           fetch('http://localhost:8000/user', options)
+            .then(res => res.json())
+            .then(data => console.log(data))
+        } catch (error) {
+            console.error('Error al realizar la solicitud:', error);
+        }
+    };
+    
     return (
         <div className="container">
             <div className="registro">
                 <h1>Registro</h1>
 
-                <form onSubmit={handleSubmit((values) => {
-                    console.log(values);
-                })}>
+                <form onSubmit={handleSubmit}>
 
-                    <input type="text" {...register("username", { required: true })}
+                    <input 
+                        type="email" 
+                        onChange={(e) => setEmail(e.target.value)} 
+                        value={email}
 
-                        placeholder='Name' />
-
-
-                    <input type="email" {...register("email", { required: true })}
                         placeholder='Email' />
 
-                    <input type="password" {...register("password", { required: true })}
+
+                    <input 
+                        type="password" 
+                        onChange={(e) => setPassword(e.target.value)} 
+                        value={password}
+
                         placeholder='Password' />
 
 
@@ -31,6 +59,8 @@ function RegisterPage() {
                     <button type='submit' className='button'>
                         Register
                     </button>
+
+
                 </form>
 
             </div>
